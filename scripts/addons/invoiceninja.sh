5ace788288f2e3821a8c59fe01750dc05aff1b0b
Miguel Emmara <62009261+MiguelRyf@users.noreply.github.com>
2021-03-06 17:50:24 +1300

    Update invoiceninja.sh

--- scripts/addons/invoiceninja.sh
+++ scripts/addons/invoiceninja.sh
@@ -116,7 +116,7 @@ ninjaDatabase='ninja'
 ninjaUser='ninja'
 ninjaPassword='ninja'
 mysql -uroot <<MYSQL_SCRIPT
-CREATE SCHEMA `$ninjaDatabase` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE SCHEMA $ninjaDatabase DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$ninjaUser'@'localhost' IDENTIFIED BY '$ninjaPassword';
 GRANT ALL PRIVILEGES ON `$ninjaDatabase`.* TO '$ninjaUser'@'localhost';
 FLUSH PRIVILEGES;
